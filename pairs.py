4a76cc9f54b85c422fc5e54371973a03314c6b13
MarKr17 <mariakroczek199@gmail.com>
2022-05-30 10:33:49 +0200

    done

--- pairs.py
+++ pairs.py
@@ -4,7 +4,7 @@ from Bio.PDB import PDBParser
 from Bio.PDB.PDBIO import PDBIO
 import math
 parser=PDBParser()
-structure=parser.get_structure("rp01","rp/rp01.pdb")
+structure=parser.get_structure("rp02","rp/rp02.pdb")
 print(structure)
 model= structure.get_models()
 print(model)
@@ -25,10 +25,8 @@ G_C={'N1_N3': [2.67, 3.19],'N2_O2':[2.44, 3.20], 'O6_N4':[2.61, 3.31]}
 
 print(residueA[0].get_resname())
 print(chains[0].get_id())
-G_A=[]
-C_A=[]
-G_B=[]
-C_B=[]
+G_list=[]
+C_list=[]
 
 wynik=[]
 for model in structure:
@@ -36,20 +34,16 @@ for model in structure:
 		for residue in chain:
 			name=residue.get_resname().strip()
 			id=chain.get_id().strip()
-			if name=='G' and id=='A':
-				G_A.append(residue)
-			if name=='C' and id=='A':
-				C_A.append(residue)
-			if name=='G' and id=='B':
-				G_B.append(residue)
-			if name=='C' and id=='B':
-				C_B.append(residue)
+			if name=='G':
+				G_list.append(residue)
+			if name=='C':
+				C_list.append(residue)
 
-
-print(G_A[0]["N1"].get_coord())
-
-for G in G_A:
-	for C in C_B:
+print(G_list[0]["N1"].get_coord())
+print(G_list[0].get_id())
+print(C_list[0]["N3"].get_coord())
+for G in G_list:
+	for C in C_list:
 		N1= G['N1'].get_coord()
 		N3= C['N3'].get_coord()
 		N1_N3= math.sqrt(pow(N1[0]-N3[0],2)+pow(N1[1]-N3[1],2)+pow(N1[2]-N3[2],2))
@@ -62,25 +56,9 @@ for G in G_A:
 				N4=C["N4"].get_coord()
 				O6_N4=math.sqrt(pow(O6[0]-N4[0],2)+pow(O6[1]-N4[1],2)+pow(O6[2]-N4[2],2))
 				if 2.61 <= O6_N4 <= 3.31:
-					s="{}:{}{} - {}:{}{}".format("A",'G',G.get_id()[1], "B","C",C.get_id()[1])
-					wynik.append(s)
-
-for G in G_B:
-	for C in C_A:
-		N1= G['N1'].get_coord()
-		N3= C['N3'].get_coord()
-		N1_N3= math.sqrt(pow(N1[0]-N3[0],2)+pow(N1[1]-N3[1],2)+pow(N1[2]-N3[2],2))
-		if 2.67 <= N1_N3 <=3.19:
-			N2=G["N2"].get_coord()
-			O2=C["O2"].get_coord()
-			N2_O2=math.sqrt(pow(N2[0]-O2[0],2)+pow(N2[1]-O2[1],2)+pow(N2[2]-O2[2],2))
-			if 2.44 <= N2_O2 <= 3.20:
-				O6=G["O6"].get_coord()
-				N4=C["N4"].get_coord()
-				O6_N4=math.sqrt(pow(O6[0]-N4[0],2)+pow(O6[1]-N4[1],2)+pow(O6[2]-N4[2],2))
-				if 2.61 <= O6_N4 <= 3.31:
-					s="{}:{}{} - {}:{}{}".format("A",'C',C.get_id()[1], "B","G",G.get_id()[1])
-					wynik.append(s)
+					s1="{}:{}{}".format(G.get_parent().get_id(),'G',G.get_id()[1])
+					s2="{}:{}{}".format(C.get_parent().get_id(),"C",C.get_id()[1])
+					wynik.append(sorted([s1,s2]))
 
 wynik=sorted(wynik)
 print(wynik)
